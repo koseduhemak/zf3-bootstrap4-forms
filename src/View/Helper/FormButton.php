1547a5b162ac1e0d1a3bfe45fc247ff6df0c349e
Maximilian Füsslin <maximilian.fuesslin@lotsearch.de>
2018-10-26 10:42:12 +0200

    fixed rendering of fontawesome icons

--- src/View/Helper/FormButton.php
+++ src/View/Helper/FormButton.php
@@ -3,9 +3,13 @@
 namespace Zf3Bootstrap4Forms\View\Helper;
 
 use Zend\Form\ElementInterface;
+use Zend\Form\Exception\InvalidArgumentException;
 
 class FormButton extends \Zend\Form\View\Helper\FormButton
 {
+    const ICON_PREPREND = 'prepend';
+    const ICON_APPEND   = 'append';
+
     public function render(ElementInterface $element, $buttonContent = null)
     {
         if (null === $buttonContent) {
@@ -20,26 +24,48 @@ class FormButton extends \Zend\Form\View\Helper\FormButton
         }
 
         // add btn class
-        if (!preg_match('/(^|[^a-z]+)btn($|[^a-z-]+)/i', $element->getAttribute('class'))) {
-            $element->setAttribute('class', trim('btn '.$element->getAttribute('class')));
+        if (! preg_match('/(^|[^a-z]+)btn($|[^a-z-]+)/i', $element->getAttribute('class'))) {
+            $element->setAttribute('class', trim('btn ' . $element->getAttribute('class')));
         }
 
-        if (!preg_match('/(^|[^a-z]+)btn-($|[^a-z]+)/i', $element->getAttribute('class'))) {
-            $element->setAttribute('class', trim($element->getAttribute('class').' btn-primary'));
+        if (! preg_match('/(^|[^a-z]+)btn-($|[^a-z]+)/i', $element->getAttribute('class'))) {
+            $element->setAttribute('class', trim($element->getAttribute('class') . ' btn-primary'));
         }
 
-        if ($icon = $element->getOption('fontawesome')) {
+        if ($fontAwesome = $element->getOption('fontawesome')) {
+            $position = static::ICON_PREPREND;
+
+            if (is_array($fontAwesome)) {
+                if (array_key_exists('position', $fontAwesome) && $fontAwesome['position'] === static::ICON_APPEND) {
+                    $position = static::ICON_APPEND;
+                }
+
+                if (! array_key_exists('icon', $fontAwesome)) {
+                    throw new InvalidArgumentException(sprintf('No icon provided. Please provide an icon (class) to show for element "%s".', $element->getName()));
+                }
+
+                $iconClass = $fontAwesome['icon'];
+            } else {
+                $iconClass = $fontAwesome;
+            }
+
             $label = '<i class="%s"></i> ';
-            $label = sprintf($label, $icon);
+            $label = sprintf($label, $iconClass);
 
             $element->setLabelOption('disable_html_escape', true);
 
-            $buttonContent = $label . $buttonContent;
+            switch ($position) {
+                case static::ICON_APPEND:
+                    $buttonContent = $buttonContent . $label;
+                    break;
+                case static::ICON_PREPREND:
+                default:
+                    $buttonContent = $label . $buttonContent;
+            }
 
-            return parent::render($element, $buttonContent);
-        } else {
             return parent::render($element, $buttonContent);
         }
-    }
 
+        return parent::render($element, $buttonContent);
+    }
 }
